5f4bd0fce7b5144d52e62df60cfd95a1d758405a
Natália da Silva Guimarães <nataliagui284@gmail.com>
2024-06-12 15:32:37 -0300

    Projeto de locadora de séries

---
+++ database/migrations/2022_02_28_230105_create_series_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('series')) {
+            Schema::create('series', function (Blueprint $table) {
+                $table->id();
+                $table->string('nome');
+                $table->timestamps();
+            });
+        }
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+    
+};
